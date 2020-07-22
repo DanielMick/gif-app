6f198135ec2cc0330425bd66406f714e4af5aff6
Daniel Herrera <danielmhrite@gmail.com>
2020-07-22 18:43:04 -0500

    docs agregado

---
+++ docs/precache-manifest.8863ced1e78a8fd94cdded27619ae985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d259d4f8ec53a2cc59e87723e6016d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e06356c578343486d5e4",
+    "url": "/static/css/main.b8663863.chunk.css"
+  },
+  {
+    "revision": "fc8297614e4f448f12ef",
+    "url": "/static/js/2.969b929d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.969b929d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06356c578343486d5e4",
+    "url": "/static/js/main.8ffd254a.chunk.js"
+  },
+  {
+    "revision": "ded719702d9692d2c54c",
+    "url": "/static/js/runtime-main.6ecacd89.js"
+  }
+]);
